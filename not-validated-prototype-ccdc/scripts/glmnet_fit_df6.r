916454ac1e195472ac449564a3d6d2e0d57aa24d
Song Guo <sguo@usgs.gov>
2015-04-06 00:47:07 +0000

    CCDC fixed auto_ts_fit return 256 error

--- not-validated-prototype-ccdc/scripts/glmnet_fit_df6.r
+++ not-validated-prototype-ccdc/scripts/glmnet_fit_df6.r
@@ -3,6 +3,7 @@ library('glmnet')
 x=read.table("glmnet_fit_inputs.txt",sep=",",col.names=c("x1","x2","x3","x4","x5","y"))
 output<-as.matrix(data.frame(x$x1,x$x2,x$x3,x$x4,x$x5))
 fit1<-glmnet(output,x$y)
+print(coef(fit1))
 
 cfs0<-coef(fit1)["(Intercept)",15]
 cfs1<-coef(fit1)["x.x1",15]
@@ -12,4 +13,5 @@ cfs2<-coef(fit1)["x.x4",15]
 cfs3<-coef(fit1)["x.x5",15]
 
 cfs<-data.frame(cfs0,cfs1,cfs2,cfs3,cfs4,cfs5)
+print(cfs)
 write.table(cfs,"glmnet_fit_outputs.txt",sep=" ",row.names=FALSE,col.names=FALSE)
--- scripts/glmnet_fit_df2.r
+++ scripts/glmnet_fit_df2.r
@@ -3,9 +3,11 @@ library('glmnet')
 x=read.table("glmnet_fit_inputs.txt",sep=",",col.names=c("x1","y"))
 output<-as.matrix(data.frame(x$x1))
 fit1<-glmnet(output,x$y)
+print(coef(fit1))
 
 cfs0<-coef(fit1)["(Intercept)",15]
 cfs1<-coef(fit1)["x.x1",15]
 
 cfs<-data.frame(cfs0,cfs1)
+print(cfs)
 write.table(cfs,"glmnet_fit_outputs.txt",sep=" ",row.names=FALSE,col.names=FALSE)
