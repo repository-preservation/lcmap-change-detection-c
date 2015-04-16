e86017e44304ab3d9af649a36daddaeeef72fb38
Song Guo <sguo@usgs.gov>
2015-04-16 21:18:52 +0000

    Fixed CCDC glment fixed lambda value

--- not-validated-prototype-ccdc/scripts/glmnet_fit_df2.r
+++ not-validated-prototype-ccdc/scripts/glmnet_fit_df2.r
@@ -2,11 +2,11 @@ library('glmnet')
 
 x=read.table("glmnet_fit_inputs.txt",sep=",",col.names=c("x1","y"))
 output<-as.matrix(data.frame(x$x1))
-fit1<-glmnet(output,x$y)
+fit1<-glmnet(output, x$y, nlambda = 1, lambda = 20, alpha = 1)
 print(coef(fit1))
 
-cfs0<-coef(fit1)["(Intercept)",15]
-cfs1<-coef(fit1)["x.x1",15]
+cfs0<-coef(fit1)["(Intercept)",1]
+cfs1<-coef(fit1)["x.x1",1]
 
 cfs<-data.frame(cfs0,cfs1)
 print(cfs)
